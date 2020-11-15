882fda8d74bfb162f4836da3e1473a3140e6cca2
Ny Thyatdora <nythyatdora@gmail.com>
2020-11-15 21:45:46 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [
+        './src/**/*.jsx',
+        './src/**/*.js',
+        './src/**/*.tsx',
+        './src/**/*.ts',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
